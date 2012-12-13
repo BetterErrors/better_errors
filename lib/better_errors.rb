1379d6565d35c1ec64ea7f0df50a79b6217d30ee
Peterson Ferreira <petersonferreiras@gmail.com>
2012-12-13 02:29:09 -0200

    Note on some methods to do code reading more fun

--- lib/better_errors.rb
+++ lib/better_errors.rb
@@ -17,12 +17,21 @@ class << BetterErrors
 
   alias_method :binding_of_caller_available?, :binding_of_caller_available
 
+  # Return default url for open in
+  # editor functionality.
+  # Can override by setting
+  # BetterErrors.editor = Proc.new{|file, line|
+  #   "mvim://open/?url=file://#{URI.escape file}&line=#{line}"
+  # }
+  #
   def editor
-    # default to opening files in TextMate
     @editor || proc { |file, line| "txmt://open/?url=file://#{URI.encode_www_form_component(file)}&line=#{line}" }
   end
 end
 
+# Check if the gem binding_of_caller
+# is available on Gemfile
+#
 begin
   require "binding_of_caller"
   BetterErrors.binding_of_caller_available = true
@@ -30,6 +39,9 @@ rescue LoadError => e
   BetterErrors.binding_of_caller_available = false
 end
 
+# Display message if 'binding_of_caller'
+# is not on Gemfile
+#
 unless BetterErrors.binding_of_caller_available?
   warn "BetterErrors: binding_of_caller gem unavailable, cannot display local variables on error pages."
   warn "Add 'binding_of_caller' to your Gemfile to make this warning go away."

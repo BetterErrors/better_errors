9e706444f60e84577309adcba1cc11caa000049b
Charlie Somerville <charlie@charliesomerville.com>
2013-02-05 23:11:04 +1100

    add BetterErrors.use_pry! option (related to #3)

--- lib/better_errors.rb
+++ lib/better_errors.rb
@@ -101,6 +101,14 @@ module BetterErrors
       end
     end
   end
+
+  # Enables experimental Pry support in the inline REPL
+  #
+  # If you encounter problems while using Pry, *please* file a bug report at
+  # https://github.com/charliesome/better_errors/issues
+  def self.use_pry!
+    REPL::PROVIDERS.unshift const: :Pry, impl: "better_errors/repl/pry"
+  end
   
   BetterErrors.editor = :textmate
 end

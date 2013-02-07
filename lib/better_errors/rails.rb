3dbe26b03fb526accffbd675b73a01304f62c761
Charlie Somerville <charlie@charliesomerville.com>
2013-02-07 20:35:13 +1100

    only insert after ActionDispatch::DebugExceptions if it is defined (fixes #119)

--- lib/better_errors/rails.rb
+++ lib/better_errors/rails.rb
@@ -3,9 +3,17 @@ module BetterErrors
   class Railtie < Rails::Railtie
     initializer "better_errors.configure_rails_initialization" do
       unless Rails.env.production?
-        Rails.application.middleware.insert_after ActionDispatch::DebugExceptions, BetterErrors::Middleware
+        insert_middleware
         BetterErrors.logger = Rails.logger
         BetterErrors.application_root = Rails.root.to_s
+      end
+    end
+
+    def insert_middleware
+      if defined? ActionDispatch::DebugExceptions
+        Rails.application.middleware.insert_after ActionDispatch::DebugExceptions, BetterErrors::Middleware
+      else
+        Rails.application.middleware.use BetterErrors::Middleware
       end
     end
   end

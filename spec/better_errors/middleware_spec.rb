1434dc0f98c4d08e6300e422293d73e68a1f706d
☈king <rking@sharpsaw.org>
2013-02-16 16:24:02 +0000

    Erasing bad whitespace

--- spec/better_errors/middleware_spec.rb
+++ spec/better_errors/middleware_spec.rb
@@ -25,46 +25,46 @@ module BetterErrors
 
     context "when requesting the /__better_errors manually" do
       let(:app) { Middleware.new(->env { ":)" }) }
-      
+
       it "should show that no errors have been recorded" do
         status, headers, body = app.call("PATH_INFO" => "/__better_errors")
         body.join.should match /No errors have been recorded yet./
       end
     end
-    
+
     context "when handling an error" do
       let(:app) { Middleware.new(->env { raise "oh no :(" }) }
-    
+
       it "should return status 500" do
         status, headers, body = app.call({})
-      
+
         status.should == 500
       end
-    
+
       it "should return UTF-8 error pages" do
         status, headers, body = app.call({})
-        
+
         headers["Content-Type"].should match /charset=utf-8/
       end
 
       it "should return text pages by default" do
         status, headers, body = app.call({})
-        
+
         headers["Content-Type"].should match /text\/plain/
       end
-      
+
       it "should return HTML pages by default" do
         # Chrome's 'Accept' header looks similar this.
         status, headers, body = app.call("HTTP_ACCEPT" => "text/html,application/xhtml+xml;q=0.9,*/*")
-        
+
         headers["Content-Type"].should match /text\/html/
       end
-      
+
       it "should log the exception" do
         logger = Object.new
         logger.should_receive :fatal
         BetterErrors.stub!(:logger).and_return(logger)
-        
+
         app.call({})
       end
     end

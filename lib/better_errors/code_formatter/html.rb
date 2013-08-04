fe9fbba04582316f08036cb5743d98402af51fa5
antn <anton@antn.me>
2013-08-03 20:34:05 -0700

    separate line numbers separate from code to allow copying, closes #42
    fixing wrong style for formatted_nums block
    
    whoops
    
    fix issue where there would be a gap if a scrollbar was needed in code box

--- lib/better_errors/code_formatter/html.rb
+++ lib/better_errors/code_formatter/html.rb
@@ -8,12 +8,19 @@ module BetterErrors
     def formatted_lines
       each_line_of(highlighted_lines) { |highlight, current_line, str|
         class_name = highlight ? "highlight" : ""
-        sprintf '<pre class="%s">%5d %s</pre>', class_name, current_line, str
+        sprintf '<pre class="%s">%s</pre>', class_name, str
+      }
+    end
+    
+    def formatted_nums
+      each_line_of(highlighted_lines) { |highlight, current_line, str|
+        class_name = highlight ? "highlight" : ""
+        sprintf '<span class="%s">%5d</span>', class_name, current_line
       }
     end
 
     def formatted_code
-      %{<div class="code">#{super}</div>}
+      %{<div class="code_linenums">#{formatted_nums.join}</div><div class="code">#{super}</div>}
     end
   end
 end

71a3bfa7db437eb257b333f792cf599909ae9446
Charlie Somerville <charlie@charliesomerville.com>
2013-09-09 00:38:33 +1000

    avoid installing binding_of_caller if it's already installed

--- ext/mkrf_conf.rb
+++ ext/mkrf_conf.rb
@@ -1,7 +1,9 @@
-require 'rubygems/dependency_installer' 
+require 'rubygems/dependency_installer'
 
 gem = Gem::DependencyInstaller.new
 
 if ["rbx", "ruby"].include?(RUBY_ENGINE)
-  gem.install "binding_of_caller", "0.7.2"
+  if Gem::Dependency.new("binding_of_caller", "0.7.2").matching_specs.empty?
+    gem.install "binding_of_caller", "0.7.2"
+  end
 end

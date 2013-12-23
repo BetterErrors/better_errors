d2686c2f3cdf4b76fe67ac0c9a427cc32a9c13a0
Charlie Somerville <charlie@charliesomerville.com>
2013-12-23 13:35:29 +1100

    files under vendor/ should not be treated as application frames

--- lib/better_errors/stack_frame.rb
+++ lib/better_errors/stack_frame.rb
@@ -48,23 +48,24 @@ module BetterErrors
     def self.has_binding_stack?(exception)
       exception.respond_to?(:__better_errors_bindings_stack) && exception.__better_errors_bindings_stack.any?
     end
-    
+
     attr_reader :filename, :line, :name, :frame_binding
-    
+
     def initialize(filename, line, name, frame_binding = nil)
       @filename       = filename
       @line           = line
       @name           = name
       @frame_binding  = frame_binding
-      
+
       set_pretty_method_name if frame_binding
     end
-    
+
     def application?
-      root = BetterErrors.application_root
-      filename.index(root) == 0 if root
+      if root = BetterErrors.application_root
+        filename.index(root) == 0 && filename.index("#{root}/vendor") != 0
+      end
     end
-    
+
     def application_path
       filename[(BetterErrors.application_root.length+1)..-1]
     end
@@ -72,7 +73,7 @@ module BetterErrors
     def gem?
       Gem.path.any? { |path| filename.index(path) == 0 }
     end
-    
+
     def gem_path
       if path = Gem.path.detect { |path| filename.index(path) == 0 }
         gem_name_and_version, path = filename.sub("#{path}/gems/", "").split("/", 2)
@@ -88,7 +89,7 @@ module BetterErrors
     def method_name
       @method_name || @name
     end
-    
+
     def context
       if gem?
         :gem
@@ -98,7 +99,7 @@ module BetterErrors
         :dunno
       end
     end
-    
+
     def pretty_path
       case context
       when :application;  application_path
@@ -106,7 +107,7 @@ module BetterErrors
       else                filename
       end
     end
-    
+
     def local_variables
       return {} unless frame_binding
       frame_binding.eval("local_variables").each_with_object({}) do |name, hash|
@@ -122,7 +123,7 @@ module BetterErrors
         end
       end
     end
-    
+
     def instance_variables
       return {} unless frame_binding
       Hash[visible_instance_variables.map { |x|
@@ -137,7 +138,7 @@ module BetterErrors
     def to_s
       "#{pretty_path}:#{line}:in `#{name}'"
     end
-    
+
   private
     def set_pretty_method_name
       return if RUBY_VERSION < "2.0.0"

aee8c09628448ffe7bac1a796234f87c360831b9
Charlie Somerville <charlie@charliesomerville.com>
2014-02-24 23:56:59 +1100

    use Module#prepend for Exception monkey patch

--- lib/better_errors/core_ext/exception.rb
+++ lib/better_errors/core_ext/exception.rb
@@ -1,21 +1,24 @@
-class Exception
-  original_set_backtrace = instance_method(:set_backtrace)
-  
-  if BetterErrors.binding_of_caller_available?
-    define_method :set_backtrace do |*args|
-      unless Thread.current[:__better_errors_exception_lock]
-        Thread.current[:__better_errors_exception_lock] = true
-        begin
-          @__better_errors_bindings_stack = binding.callers.drop(1)
-        ensure
-          Thread.current[:__better_errors_exception_lock] = false
+module BetterErrors
+  module CoreExt
+    module Exception
+      def set_backtrace(*)
+        unless Thread.current[:__better_errors_exception_lock]
+          Thread.current[:__better_errors_exception_lock] = true
+          begin
+            @__better_errors_bindings_stack = binding.callers.drop(1)
+          ensure
+            Thread.current[:__better_errors_exception_lock] = false
+          end
         end
+
+        super
       end
-      original_set_backtrace.bind(self).call(*args)
+
+      def __better_errors_bindings_stack
+        @__better_errors_bindings_stack || []
+      end
     end
   end
-  
-  def __better_errors_bindings_stack
-    @__better_errors_bindings_stack || []
-  end
 end
+
+Exception.send(:prepend, BetterErrors::CoreExt::Exception)

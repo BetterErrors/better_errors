88a7ceaf18f13825c993177e488959852d009d17
Cory Fabre <cfabre+github@gmail.com>
2014-08-07 15:33:29 -0500

    Use Rack Request to check proxy forwarded IPs

--- lib/better_errors/middleware.rb
+++ lib/better_errors/middleware.rb
@@ -62,10 +62,9 @@ module BetterErrors
   private
 
     def allow_ip?(env)
-      # REMOTE_ADDR is not in the rack spec, so some application servers do
-      # not provide it.
-      return true unless env["REMOTE_ADDR"] and !env["REMOTE_ADDR"].strip.empty?
-      ip = IPAddr.new env["REMOTE_ADDR"].split("%").first
+      request = Rack::Request.new(env)
+      return true unless request.ip and !request.ip.strip.empty?
+      ip = IPAddr.new request.ip.split("%").first
       ALLOWED_IPS.any? { |subnet| subnet.include? ip }
     end
 

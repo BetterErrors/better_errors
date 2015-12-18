a05a894134d62002e88f1e951beca90d2ddd06fa
Charlie Somerville <charlie@charliesomerville.com>
2015-12-18 17:08:20 +1100

    ignore special #$! local variable in StackFrame#local_variables

--- lib/better_errors/stack_frame.rb
+++ lib/better_errors/stack_frame.rb
@@ -68,7 +68,14 @@ module BetterErrors
 
     def local_variables
       return {} unless frame_binding
+
       frame_binding.eval("local_variables").each_with_object({}) do |name, hash|
+        # Ruby 2.2's local_variables will include the hidden #$! variable if
+        # called from within a rescue context. This is not a valid variable name,
+        # so the local_variable_get method complains. This should probably be
+        # considered a bug in Ruby itself, but we need to work around it.
+        next if name == :"\#$!"
+
         if defined?(frame_binding.local_variable_get)
           hash[name] = frame_binding.local_variable_get(name)
         else

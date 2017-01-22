b8b84841b8ec7ed1afca706d9786402fcd977158
Serghei Iakovlev <sergeyklay@users.noreply.github.com>
2017-01-22 23:32:10 +0200

    Update better_errors.rb

--- lib/better_errors.rb
+++ lib/better_errors.rb
@@ -17,6 +17,7 @@ module BetterErrors
     { symbols: [:macvim, :mvim],        sniff: /vim/i,   url: proc { |file, line| "mvim://open?url=file://#{file}&line=#{line}" } },
     { symbols: [:sublime, :subl, :st],  sniff: /subl/i,  url: "subl://open?url=file://%{file}&line=%{line}" },
     { symbols: [:textmate, :txmt, :tm], sniff: /mate/i,  url: "txmt://open?url=file://%{file}&line=%{line}" },
+    { symbols: [:idea],                 sniff: /idea/i,  url: "idea://open?file=%{file}&line=%{line}" },
   ]
 
   class << self

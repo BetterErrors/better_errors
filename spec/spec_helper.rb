d5fe4969423b992138b39047f9ccb4ed50046296
Robin Daugherty <robin@robindaugherty.net>
2017-08-25 11:26:49 -0400

    Add coveralls with parallel build processing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,5 +2,10 @@ $: << File.expand_path("../../lib", __FILE__)
 
 ENV["EDITOR"] = nil
 
+require 'coveralls'
+Coveralls.wear! do
+  add_filter 'spec/'
+end
+
 require 'bundler/setup'
 Bundler.require(:default)

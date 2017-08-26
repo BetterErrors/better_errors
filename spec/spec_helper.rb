23566b1b3c997686882812c1956473c46cbd6f82
Robin Daugherty <robin@robindaugherty.net>
2017-08-25 20:01:55 -0400

    Disable coverage reporting with ruby 2.4.0/2.4.1

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,9 +2,14 @@ $: << File.expand_path("../../lib", __FILE__)
 
 ENV["EDITOR"] = nil
 
-require 'coveralls'
-Coveralls.wear! do
-  add_filter 'spec/'
+# Ruby 2.4.0 and 2.4.1 has a bug with its Coverage module that causes segfaults.
+# https://bugs.ruby-lang.org/issues/13305
+# 2.4.2 should include this patch.
+unless RUBY_VERSION == '2.4.0' || RUBY_VERSION == '2.4.1'
+  require 'coveralls'
+  Coveralls.wear! do
+    add_filter 'spec/'
+  end
 end
 
 require 'bundler/setup'

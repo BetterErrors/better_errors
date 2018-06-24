f3afff13460564270473b08d84c0c3a4de395d6a
Robin Daugherty <robin@robindaugherty.net>
2018-06-23 22:19:46 -0400

    Generate local coverage
    and save Coveralls for CI.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,11 +5,16 @@ ENV["EDITOR"] = nil
 # Ruby 2.4.0 and 2.4.1 has a bug with its Coverage module that causes segfaults.
 # https://bugs.ruby-lang.org/issues/13305
 # 2.4.2 should include this patch.
-unless RUBY_VERSION == '2.4.0' || RUBY_VERSION == '2.4.1'
-  require 'coveralls'
-  Coveralls.wear! do
-    add_filter 'spec/'
+if ENV['CI']
+  unless RUBY_VERSION == '2.4.0' || RUBY_VERSION == '2.4.1'
+    require 'coveralls'
+    Coveralls.wear! do
+      add_filter 'spec/'
+    end
   end
+else
+  require 'simplecov'
+  SimpleCov.start
 end
 
 require 'bundler/setup'

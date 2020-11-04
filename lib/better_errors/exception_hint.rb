a557972d642c614b57dbd46666dbf1a97b8341d9
Robin Daugherty <robin@robindaugherty.net>
2020-11-04 12:55:15 -0500

    Fix passing regex to start_with?

--- lib/better_errors/exception_hint.rb
+++ lib/better_errors/exception_hint.rb
@@ -10,7 +10,7 @@ module BetterErrors
         /\Aundefined method `(?<method>[^']+)' for (?<val>[^:]+):(?<klass>\w+)/.match(exception.message) do |match|
           if match[:val] == "nil"
             return "Something is `nil` when it probably shouldn't be."
-          elsif !match[:klass].start_with? /\A0x/
+          elsif !match[:klass].start_with? '0x'
             return "`#{match[:method]}` is being called on a `#{match[:klass]}` object, "\
               "which might not be the type of object you were expecting."
           end

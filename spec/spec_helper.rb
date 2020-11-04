db589f2463d274f54a003442a11858bd6dc546af
Robin Daugherty <robin@robindaugherty.net>
2020-11-04 09:14:44 -0500

    Fix simplecov-lcov incompatible with older simplecov
    (which is required to support Ruby < 2.4)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,16 @@ ENV["EDITOR"] = nil
 require 'simplecov'
 require 'simplecov-lcov'
 
+# Fix incompatibility of simplecov-lcov with older versions of simplecov that are not expresses in its gemspec.
+# https://github.com/fortissimo1997/simplecov-lcov/pull/25
+if !SimpleCov.respond_to?(:branch_coverage)
+  module SimpleCov
+    def self.branch_coverage?
+      false
+    end
+  end
+end
+
 SimpleCov::Formatter::LcovFormatter.config do |c|
   c.report_with_single_file = true
   c.single_report_path = 'coverage/lcov.info'
